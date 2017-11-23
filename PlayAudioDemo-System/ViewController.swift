f4b25161b10eb9e1d9698eb7849cc284360fd567
derek <derek@butterfly.sg>
2017-11-23 12:13:46 +0800

    Init

---
+++ PlayAudioDemo-System/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayAudioDemo-System
+//
+//  Created by Julie on 23/11/17.
+//  Copyright © 2017 ZHOU DENGFENG DEREK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
